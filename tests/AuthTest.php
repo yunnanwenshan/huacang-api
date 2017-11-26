ad153d37f8fb9f52deca805819b6d8878611efce
liutianping <liutianping@ttyongche.com>
2017-11-26 14:42:51 +0800

    修改配置

--- tests/AuthTest.php
+++ tests/AuthTest.php
@@ -7,7 +7,7 @@ class AuthTest extends TestCase
     private $client;
     private $jar;
 
-    const LOCAL_SERVER = 'http://127.0.0.1:8888';
+    const LOCAL_SERVER = 'http://api.huacang.com';
     static $current_server = self::LOCAL_SERVER;
 
     public function __construct()
